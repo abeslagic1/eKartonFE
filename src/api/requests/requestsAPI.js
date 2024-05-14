a622430be5037720dff4875ac169274944faf504
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2024-05-14 15:54:15 +0200

    dodatni zakomentarisani request (JWT, kasnije)

--- src/api/requests/requestsAPI.js
+++ src/api/requests/requestsAPI.js
@@ -20,7 +20,9 @@ headers: { 'Authorization': 'Basic ' + Buffer.from(username + ":" + password).to
 In this code, 'Authorization': 'Basic ' + Buffer.from(username + ":" + password).toString('base64') and 'Bearer': window.localStorage.token are added as separate properties in the headers of the request. This sends both Basic Auth and Bearer token in the headers of the request. */
 
 
-export async function addFileToFavourites(fileId) {
+/*
+viska request, dodavanje Bearer tokena (trebat ce kasnije)
+ export async function addFileToFavourites(fileId) {
     return await resolve(
         axios({
             method: 'post',
@@ -29,6 +31,7 @@ export async function addFileToFavourites(fileId) {
             headers: { 'Authorization': 'Bearer ' + window.localStorage.token }
         }));
 }
+ */
 
 export async function getRequests(options) {
     let encodedCredentials = encodeCredentials(backend_username, backend_password);
